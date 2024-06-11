7949f6057bb8bce38b61f23a0cb198ec8dbd56ce
许盛 <20691718@qq.com>
2024-06-11 11:32:19 +0800

    chore: 尝试使用 backend 类型

--- apps/frontend/src/api/user.ts
+++ apps/frontend/src/api/user.ts
@@ -1,19 +1,20 @@
-import { Res } from '../interface/res.ts'
-import { User } from '../interface/user.ts'
 import { request } from './request.tsx'
+import { ResFromController } from '../interface/helper.ts'
+import { UserController } from 'backend/dist/module/user/user.controller.ts'
+import { UpdateUsernameDto } from 'backend/dist/module/user/dto/update-username.dto'
 
 export class UserApi {
   private readonly baseUrl = '/api/user'
 
-  me = () => request<Res<User>>(`${this.baseUrl}/me`)
+  me = () => request<ResFromController<UserController['getMe']>>(`${this.baseUrl}/me`)
 
   // 修改当前登录用户密码
   updateMePassword = (data: { oldPassword: string; newPassword: string }) =>
     request.put(`${this.baseUrl}/me/update-password`, data)
 
   // 修改当前登录用户用户名
-  updateMeUserInfo = (data: { username: string }) =>
-    request.put(`${this.baseUrl}/me/update-username`, data)
+  updateMeUserInfo = (data: UpdateUsernameDto) =>
+    request.put<ResFromController<UserController['updateMeUsername']>>(`${this.baseUrl}/me/update-username`, data)
 }
 
 export const userApi = new UserApi()
---
+++ apps/frontend/src/interface/helper.ts
@@ -0,0 +1,29 @@
+import { AxiosResponse } from 'axios'
+import { DWithPage } from './page.ts'
+
+interface Meta {
+  errCode: number
+  errMsg: string
+  errDebug: string
+  errDetails: string[]
+}
+
+export interface Res<T extends unknown = Record<string, unknown>> extends Meta {
+  data: T
+}
+
+// 自动中后端定义的类中的某个方法返回的 Promise 或非 Promise 中提取出类型
+export type ResFromController<T extends (...args: any[]) => any> = Res<
+  Awaited<ReturnType<T>>
+>
+
+// 自动从接口返回中提取 data 类型
+export type ExtractFromRes<
+  T extends (...args: any[]) => Promise<AxiosResponse<Res<unknown>>>
+> = T extends (...args: any[]) => Promise<AxiosResponse<Res<infer R>>>
+  ? R extends Array<infer P>
+    ? P
+    : R extends DWithPage<infer S>
+      ? S
+      : never
+  : never
